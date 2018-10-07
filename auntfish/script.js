1ae5ec991a5cbb96f2704d9433188a275006edc6
DanLish <popovdanila2002@mail.ru>
2018-10-07 20:28:12 +0300

    auntfish_commit
    =)

---
+++ auntfish/script.js
@@ -0,0 +1,12 @@
+function a_value(o)
+{
+if (o.value == 3279313442123251210123112) {
+  document.location.href = "file:///C:/Users/popov/Desktop/auntfish/menuevgen3313212a3c1129323-b11.html";
+} else if (o.value == 321311433323122032512433112) {
+  document.location.href = "file:///C:/Users/popov/Desktop/auntfish/menuyoshkin3313323331129323-a2.html";
+} else if (o.value == 321111486712111311319323) {
+  document.location.href = "file:///C:/Users/popov/Desktop/auntfish/menudanlish33111111111212a3s113e19323-c176.html";
+} else {
+  alert( 'Неверный ключ доступа!' );
+}
+}
